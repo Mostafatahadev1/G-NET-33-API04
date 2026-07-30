dce6ab09113b22210861550a424ca963c99cf6b1
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-07-30 05:15:33 +0300

    Add project files.

---
+++ E_Commerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Get All Products 
+
+        // Get Products By Id
+
+        // Get All Types 
+
+        // Get All Brands
+    }
+}
