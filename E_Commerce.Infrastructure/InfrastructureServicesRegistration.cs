ca15e8b1bd950420307e15a506f47bc45431f637
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-08-07 08:18:42 +0300

    Basket Module - Basket Service

--- E_Commerce.Infrastructure/InfrastructureServicesRegistration.cs
+++ E_Commerce.Infrastructure/InfrastructureServicesRegistration.cs
@@ -5,6 +5,7 @@ using E_Commerce.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,14 @@ namespace E_Commerce.Infrastructure
             services.AddKeyedScoped<IDataSeeder, CatalogDataSeeder>("Catalog");
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            services.AddSingleton<IConnectionMultiplexer>(Config =>
+            {
+                return ConnectionMultiplexer.Connect(configuration.GetConnectionString("RedisConnection")!);
+            });
+
+            services.AddScoped<IBasketRepository, BasketRepository>();
             return services;
         }
 
